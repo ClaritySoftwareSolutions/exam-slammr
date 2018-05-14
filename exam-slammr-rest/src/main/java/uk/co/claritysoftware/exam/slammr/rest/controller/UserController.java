45632bbf85238370168c3053e32990e68cd7fe7d
Nathan Russell <nathan@claritysoftware.co.uk>
2018-05-14 22:05:08 +0100

    Starting to flesh out UserController a little

--- exam-slammr-rest/src/main/java/uk/co/claritysoftware/exam/slammr/rest/controller/UserController.java
+++ exam-slammr-rest/src/main/java/uk/co/claritysoftware/exam/slammr/rest/controller/UserController.java
@@ -1,17 +1,22 @@
 package uk.co.claritysoftware.exam.slammr.rest.controller;
 
+import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import com.google.common.base.Preconditions;
 import uk.co.claritysoftware.exam.slammr.rest.model.UserRegistrationRequest;
 import uk.co.claritysoftware.exam.slammr.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * REST Controller for User concerns
  */
+@Slf4j
 @RestController("/user")
 public class UserController {
 
@@ -24,7 +29,12 @@ public class UserController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<Void> registerNewUser(UserRegistrationRequest userRegistrationRequest) {
+	public ResponseEntity<Void> registerNewUser(UserRegistrationRequest userRegistrationRequest, Principal userPrincipal) {
+		Preconditions.checkArgument(userPrincipal != null, "User Principal must not be null");
+
+		String identityId = userPrincipal.getName();
+		log.debug("Register New User with UserRegistrationRequest {} with identityId {}", userRegistrationRequest, identityId);
+
 		return null;
 	}
 }
---
+++ exam-slammr-rest/src/test/java/uk/co/claritysoftware/exam/slammr/rest/controller/UserControllerTest.java
@@ -0,0 +1,19 @@
+package uk.co.claritysoftware.exam.slammr.rest.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Unit test class for {@link UserController}
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserControllerTest {
+
+	@Test
+	public void should() {
+
+	}
+}
