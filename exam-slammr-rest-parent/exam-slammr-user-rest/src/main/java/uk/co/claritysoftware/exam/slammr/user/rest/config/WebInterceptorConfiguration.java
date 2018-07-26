8b15fdaf128ca14d3632eced81d70fa4143db8fa
Nathan Russell <nathan@megster.net>
2018-07-26 16:24:23 +0100

    Minor change of interceptor ordering

--- exam-slammr-rest-parent/exam-slammr-user-rest/src/main/java/uk/co/claritysoftware/exam/slammr/user/rest/config/WebInterceptorConfiguration.java
+++ exam-slammr-rest-parent/exam-slammr-user-rest/src/main/java/uk/co/claritysoftware/exam/slammr/user/rest/config/WebInterceptorConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import uk.co.claritysoftware.exam.slammr.web.interceptor.ClearMdcInterceptor;
 import uk.co.claritysoftware.exam.slammr.web.interceptor.IdentityIdMdcInterceptor;
 
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 import static org.springframework.core.Ordered.LOWEST_PRECEDENCE;
 
 /**
@@ -16,7 +17,7 @@ public class WebInterceptorConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new IdentityIdMdcInterceptor()).order(LOWEST_PRECEDENCE);
+        registry.addInterceptor(new IdentityIdMdcInterceptor()).order(HIGHEST_PRECEDENCE);
         registry.addInterceptor(new ClearMdcInterceptor()).order(LOWEST_PRECEDENCE);
     }
 }
