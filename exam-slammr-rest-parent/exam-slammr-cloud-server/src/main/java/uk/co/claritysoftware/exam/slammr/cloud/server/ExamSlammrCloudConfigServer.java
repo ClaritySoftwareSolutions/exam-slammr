09ce6d467be9c2268b319227bdf2b4c4ba807150
Nathan Russell <nathan@claritysoftware.co.uk>
2018-07-26 20:17:06 +0100

    Basic Spring Cloud Config Server

---
+++ exam-slammr-rest-parent/exam-slammr-cloud-server/src/main/java/uk/co/claritysoftware/exam/slammr/cloud/server/ExamSlammrCloudConfigServer.java
@@ -0,0 +1,17 @@
+package uk.co.claritysoftware.exam.slammr.cloud.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Main Spring Boot Cloud Config Server bootstrapping class
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ExamSlammrCloudConfigServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSlammrCloudConfigServer.class, args);
+	}
+}
