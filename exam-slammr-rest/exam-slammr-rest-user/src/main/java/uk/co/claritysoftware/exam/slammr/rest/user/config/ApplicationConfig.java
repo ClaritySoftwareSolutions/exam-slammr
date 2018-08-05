38e2e2b90dbde224e621afb08807ad21820e74b7
Nathan Russell <nathan@claritysoftware.co.uk>
2018-08-05 21:09:32 +0100

    Fleshing out the cloud config

---
+++ exam-slammr-rest/exam-slammr-rest-user/src/main/java/uk/co/claritysoftware/exam/slammr/rest/user/config/ApplicationConfig.java
@@ -0,0 +1,24 @@
+package uk.co.claritysoftware.exam.slammr.rest.user.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+
+/**
+ * Simple bean encapsulating the application configuration properties
+ */
+@Component
+@Getter
+public class ApplicationConfig {
+
+	@Value("${aws.region}")
+	private String awsRegion;
+
+	@Value("${aws.accessKeyId}")
+	private String awsAccessKeyId;
+
+	@Value("${aws.secretKey}")
+	private String awsSecretKey;
+
+}
--- exam-slammr-rest/exam-slammr-rest-user/src/main/java/uk/co/claritysoftware/exam/slammr/rest/user/service/config/DynamoDbConfiguration.java
+++ exam-slammr-rest/exam-slammr-rest-user/src/main/java/uk/co/claritysoftware/exam/slammr/rest/user/service/config/DynamoDbConfiguration.java
@@ -1,11 +1,16 @@
 package uk.co.claritysoftware.exam.slammr.rest.user.service.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import uk.co.claritysoftware.exam.slammr.rest.user.config.ApplicationConfig;
 
 /**
  * Spring Boot configuration class for AWS DynamoDB concerns
@@ -13,12 +18,27 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 @Configuration
 public class DynamoDbConfiguration {
 
-    private static final Regions REGION = Regions.EU_WEST_2;
+	private final ApplicationConfig applicationConfig;
+
+	private final Regions region;
+
+	@Autowired
+	public DynamoDbConfiguration(ApplicationConfig applicationConfig) {
+		this.applicationConfig = applicationConfig;
+		this.region = Regions.valueOf(applicationConfig.getAwsRegion());
+	}
+
+	@Bean
+	public AWSCredentialsProvider credentialsProvider() {
+		BasicAWSCredentials awsCredentials = new BasicAWSCredentials(applicationConfig.getAwsAccessKeyId(), applicationConfig.getAwsSecretKey());
+		return new AWSStaticCredentialsProvider(awsCredentials);
+	}
 
     @Bean
-    public DynamoDBMapper dynamoDBMapper() {
+    public DynamoDBMapper dynamoDBMapper(AWSCredentialsProvider credentialsProvider) {
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                .withRegion(REGION)
+				.withCredentials(credentialsProvider)
+                .withRegion(region)
                 .build();
         return new DynamoDBMapper(client);
     }
