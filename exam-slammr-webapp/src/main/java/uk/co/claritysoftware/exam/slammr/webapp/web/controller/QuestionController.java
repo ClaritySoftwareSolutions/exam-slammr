e513fc18c1d195648b6d2390101a3c0ef3fba98e
Nathan Russell <nathan@claritysoftware.co.uk>
2018-09-14 12:13:14 +0100

    Starting to add Create Question form submission code

--- exam-slammr-webapp/src/main/java/uk/co/claritysoftware/exam/slammr/webapp/web/controller/QuestionController.java
+++ exam-slammr-webapp/src/main/java/uk/co/claritysoftware/exam/slammr/webapp/web/controller/QuestionController.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import uk.co.claritysoftware.exam.slammr.webapp.web.model.question.AnswerOption;
 import uk.co.claritysoftware.exam.slammr.webapp.web.model.question.CreateQuestion;
-import uk.co.claritysoftware.exam.slammr.webapp.web.model.question.FurtherReading;
 
 /**
  * Controller to handle question page requests
