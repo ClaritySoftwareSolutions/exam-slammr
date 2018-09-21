b8834a1312360be8ab4c9e14fd0e1a4421e0708d
Nathan Russell <nathan@megster.net>
2018-09-21 19:07:45 +0100

    Fixed test

--- exam-slammr-webapp/src/test/java/uk/co/claritysoftware/exam/slammr/webapp/web/controller/QuestionControllerMockMvcTest.java
+++ exam-slammr-webapp/src/test/java/uk/co/claritysoftware/exam/slammr/webapp/web/controller/QuestionControllerMockMvcTest.java
@@ -1,26 +1,32 @@
 package uk.co.claritysoftware.exam.slammr.webapp.web.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static uk.co.claritysoftware.exam.slammr.webapp.testsupport.web.model.question.CreateQuestionTestDataFactory.aSimpleCreateQuestionAboutTriangles;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import uk.co.claritysoftware.exam.slammr.webapp.service.QuestionService;
+import uk.co.claritysoftware.exam.slammr.webapp.service.model.question.Question;
+import uk.co.claritysoftware.exam.slammr.webapp.service.model.question.QuestionStatus;
 import uk.co.claritysoftware.exam.slammr.webapp.web.model.question.CreateQuestion;
+
+import static io.florianlopes.spring.test.web.servlet.request.MockMvcRequestBuilderUtils.postForm;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static uk.co.claritysoftware.exam.slammr.webapp.testsupport.service.model.question.QuestionTestDataFactory.aSimpleQuestionAboutTriangles;
+import static uk.co.claritysoftware.exam.slammr.webapp.testsupport.web.model.question.CreateQuestionTestDataFactory.aSimpleCreateQuestionAboutTriangles;
 
 /**
  * MockMvc test class for {@link QuestionController}
@@ -31,108 +37,108 @@ import uk.co.claritysoftware.exam.slammr.webapp.web.model.question.CreateQuestio
 @TestPropertySource({"classpath:test-context.properties"})
 public class QuestionControllerMockMvcTest {
 
-	@Autowired
-	private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-	@Test
-	public void shouldNotGetQuestionPageGivenUnauthenticatedUser() throws Exception {
-		// Given
-		RequestBuilder request = get("/question/new");
+    @MockBean
+    private QuestionService questionService;
 
-		// When
-		MockHttpServletResponse response = mvc.perform(request)
-				.andReturn().getResponse();
+    @Test
+    public void shouldNotGetQuestionPageGivenUnauthenticatedUser() throws Exception {
+        // Given
+        RequestBuilder request = get("/question/new");
 
-		// Then
-		assertThat(response.getStatus())
-				.as("HTTP Status code is 403")
-				.isEqualTo(403);
-	}
+        // When
+        MockHttpServletResponse response = mvc.perform(request)
+                .andReturn().getResponse();
 
-	@Test
-	public void shouldGetQuestionPageGivenAuthenticatedUser() throws Exception {
-		// Given
-		RequestBuilder request = get("/question/new")
-				.with(user("aUser"));
+        // Then
+        assertThat(response.getStatus())
+                .as("HTTP Status code is 403")
+                .isEqualTo(403);
+    }
 
-		// When
-		MockHttpServletResponse response = mvc.perform(request)
-				.andReturn().getResponse();
+    @Test
+    public void shouldGetQuestionPageGivenAuthenticatedUser() throws Exception {
+        // Given
+        RequestBuilder request = get("/question/new")
+                .with(user("aUser"));
 
-		// Then
-		assertThat(response.getStatus())
-				.as("HTTP Status code is 200")
-				.isEqualTo(200);
-	}
+        // When
+        MockHttpServletResponse response = mvc.perform(request)
+                .andReturn().getResponse();
 
-	@Test
-	public void shouldNotCreateNewQuestionGivenUnauthenticatedUser() throws Exception {
-		// Given
-		CreateQuestion question = aSimpleCreateQuestionAboutTriangles().build();
+        // Then
+        assertThat(response.getStatus())
+                .as("HTTP Status code is 200")
+                .isEqualTo(200);
+    }
 
-/*
-		RequestBuilder request = post("/question")
-				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-				.with(csrf());
-		addFormFieldsToRequest((MockHttpServletRequestBuilder)request, question);
-*/
-		RequestBuilder request = MockMvcRequestBuilderUtils.postForm("/question", question)
-				.with(csrf());
+    @Test
+    public void shouldNotCreateNewQuestionGivenUnauthenticatedUser() throws Exception {
+        // Given
+        CreateQuestion question = aSimpleCreateQuestionAboutTriangles().build();
 
-		// When
-		MockHttpServletResponse response = mvc.perform(request)
-				.andReturn().getResponse();
+        RequestBuilder request = postForm("/question", question)
+                .with(csrf());
 
-		// Then
-		assertThat(response.getStatus())
-				.as("HTTP Status code is 403")
-				.isEqualTo(403);
-	}
+        // When
+        MockHttpServletResponse response = mvc.perform(request)
+                .andReturn().getResponse();
 
-	@Test
-	public void shouldCreateNewQuestionGivenAuthenticatedUser() throws Exception {
-		// Given
-		CreateQuestion question = aSimpleCreateQuestionAboutTriangles()
-				.action(CreateQuestion.Action.save)
-				.build();
+        // Then
+        assertThat(response.getStatus())
+                .as("HTTP Status code is 403")
+                .isEqualTo(403);
+    }
 
-		RequestBuilder request = post("/question")
-				.with(user("aUser"))
-				.with(csrf());
-		addFormFieldsToRequest((MockHttpServletRequestBuilder)request, question);
+    @Test
+    public void shouldCreateNewQuestionGivenAuthenticatedUser() throws Exception {
+        // Given
+        String userName = "aUser";
 
-		// When
-		MockHttpServletResponse response = mvc.perform(request)
-				.andReturn().getResponse();
+        CreateQuestion createQuestion = aSimpleCreateQuestionAboutTriangles()
+                .action(CreateQuestion.Action.save)
+                .build();
 
-		// Then
-		assertThat(response.getStatus())
-				.as("HTTP Status code is 403")
-				.isEqualTo(403);
-	}
+        Question newQuestion = aSimpleQuestionAboutTriangles()
+                .id(null)
+                .slug(null)
+                .createdBy(userName)
+                .updatedBy(null)
+                .updatedDateTime(null)
+                .status(QuestionStatus.SUBMITTED_FOR_APPROVAL)
+                .votes(0)
+                .build();
+        Question savedQuestion = aSimpleQuestionAboutTriangles()
+                .createdBy(userName)
+                .updatedBy(null)
+                .updatedDateTime(null)
+                .status(QuestionStatus.SUBMITTED_FOR_APPROVAL)
+                .votes(0)
+                .build();
+        given(questionService.saveNewQuestion(any()))
+                .willReturn(savedQuestion);
 
-	private void addFormFieldsToRequest(MockHttpServletRequestBuilder request, CreateQuestion question) {
-		addFormField(request, "action", question.getAction());
-		addFormField(request, "summary", question.getSummary());
-		addFormField(request, "question", question.getQuestion());
-		
-		if (question.getTags() != null) {
-			for (int i=0; i<question.getTags().size(); i++) {
-				addFormField(request, "tags[" + i + "]", question.getTags().get(i));
-			}
-		}
-		
-		if (question.getCertifications() != null) {
-			for (int i=0; i<question.getCertifications().size(); i++) {
-				addFormField(request, "certifications[" + i + "]", question.getCertifications().get(i));
-			}
-		}
-		
-	}
+        RequestBuilder request = postForm("/question", createQuestion)
+                .with(user(userName))
+                .with(csrf());
 
-	private void addFormField(MockHttpServletRequestBuilder request, String field, Object value) {
-		if (value != null) {
-			request.param(field, value.toString());
-		}
-	}
+        // When
+        MockHttpServletResponse response = mvc.perform(request)
+                .andReturn().getResponse();
+
+        // Then
+        then(questionService).should().saveNewQuestion(argThat(q -> {
+            assertThat(q).isEqualToIgnoringGivenFields(newQuestion, "createdDateTime");
+            return true;
+        }));
+        assertThat(response.getStatus())
+                .as("HTTP Status code is 302")
+                .isEqualTo(302);
+        assertThat(response.getHeader("Location"))
+                .as("Location header is root")
+                .isEqualTo("/");
+    }
+
 }
