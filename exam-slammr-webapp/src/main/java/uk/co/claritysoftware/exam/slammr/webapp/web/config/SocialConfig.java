2df191cbb617b3062655a766b8712b8924d4e129
Nathan Russell <nathan@claritysoftware.co.uk>
2018-09-21 19:54:22 +0100

    Removed h2 and jdbc based account persistence example

--- exam-slammr-webapp/src/main/java/uk/co/claritysoftware/exam/slammr/webapp/web/config/SocialConfig.java
+++ exam-slammr-webapp/src/main/java/uk/co/claritysoftware/exam/slammr/webapp/web/config/SocialConfig.java
@@ -1,14 +1,7 @@
 package uk.co.claritysoftware.exam.slammr.webapp.web.config;
 
-import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseFactory;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.jdbc.datasource.init.DatabasePopulator;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -17,9 +10,7 @@ import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.UsersConnectionRepository;
-import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
 import org.springframework.social.connect.web.ProviderSignInUtils;
-import uk.co.claritysoftware.exam.slammr.webapp.account.JdbcAccountRepository;
 import uk.co.claritysoftware.exam.slammr.webapp.persistence.dynamodb.repository.DynamoDbUserConnectionItemRepository;
 import uk.co.claritysoftware.exam.slammr.webapp.security.springsocial.DynamoDbUsersConnectionRepository;
 
@@ -36,21 +27,7 @@ public class SocialConfig extends SocialConfigurerAdapter {
 	}
 
 	@Bean
-	public DataSource dataSource() {
-		EmbeddedDatabaseFactory factory = new EmbeddedDatabaseFactory();
-		factory.setDatabaseName("spring-social-showcase");
-		factory.setDatabaseType(EmbeddedDatabaseType.H2);
-		factory.setDatabasePopulator(databasePopulator());
-		return factory.getDatabase();
-	}
-
-	@Bean
-	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-		return new JdbcTemplate(dataSource);
-	}
-
-	@Bean
-	public UsersConnectionRepository usersConnectionRepository(DynamoDbUserConnectionItemRepository userConnectionRepository, DataSource dataSource, ConnectionFactoryLocator connectionFactoryLocator) {
+	public UsersConnectionRepository usersConnectionRepository(DynamoDbUserConnectionItemRepository userConnectionRepository, ConnectionFactoryLocator connectionFactoryLocator) {
 		return new DynamoDbUsersConnectionRepository(userConnectionRepository, connectionFactoryLocator, noOpTextEncrypter());
 		// return new JdbcUsersConnectionRepository(dataSource, connectionFactoryLocator, noOpTextEncrypter());
 	}
@@ -63,14 +40,6 @@ public class SocialConfig extends SocialConfigurerAdapter {
 	@Bean
 	public TextEncryptor noOpTextEncrypter() {
 		return Encryptors.noOpText();
-	}
-
-	private DatabasePopulator databasePopulator() {
-		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(new ClassPathResource("JdbcUsersConnectionRepository.sql", JdbcUsersConnectionRepository.class));
-		populator.addScript(new ClassPathResource("Account.sql", JdbcAccountRepository.class));
-		populator.addScript(new ClassPathResource("data.sql", JdbcAccountRepository.class));
-		return populator;
 	}
 
 }
