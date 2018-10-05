186ed631a1be197fc14ce9436cbdb1013ce6af18
Nathan Russell <nathan@megster.net>
2018-10-05 16:27:00 +0100

    Basic WIP signin impl

--- exam-slammr-webapp/src/main/java/uk/co/claritysoftware/exam/slammr/webapp/web/config/SocialConfig.java
+++ exam-slammr-webapp/src/main/java/uk/co/claritysoftware/exam/slammr/webapp/web/config/SocialConfig.java
@@ -8,9 +8,13 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
+import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.web.ProviderSignInController;
 import org.springframework.social.connect.web.ProviderSignInUtils;
+import org.springframework.social.connect.web.SignInAdapter;
+import org.springframework.web.context.request.NativeWebRequest;
 import uk.co.claritysoftware.exam.slammr.webapp.persistence.dynamodb.repository.DynamoDbUserConnectionItemRepository;
 import uk.co.claritysoftware.exam.slammr.webapp.security.springsocial.DynamoDbUsersConnectionRepository;
 
@@ -26,6 +30,19 @@ public class SocialConfig extends SocialConfigurerAdapter {
 		return new ProviderSignInUtils(connectionFactoryLocator, usersConnectionRepository);
 	}
 
+	@Bean
+	public ProviderSignInController providerSignInController(ConnectionFactoryLocator connectionFactoryLocator, UsersConnectionRepository usersConnectionRepository) {
+		return new ProviderSignInController(connectionFactoryLocator,
+				usersConnectionRepository, new SignInAdapter() {
+
+			@Override
+			public String signIn(String userId, Connection<?> connection, NativeWebRequest request) {
+
+System.out.println(userId);
+				return null;
+			}
+		});
+	}
 	@Bean
 	public UsersConnectionRepository usersConnectionRepository(DynamoDbUserConnectionItemRepository userConnectionRepository, ConnectionFactoryLocator connectionFactoryLocator) {
 		return new DynamoDbUsersConnectionRepository(userConnectionRepository, connectionFactoryLocator, noOpTextEncrypter());
